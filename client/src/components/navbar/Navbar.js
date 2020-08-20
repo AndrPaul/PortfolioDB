dce943b5e1178a8fc1c542bad47fa66d860af030
Paul Andrica <andpau96@gmail.com>
2020-08-21 00:22:42 +0200

    nav update

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import {NavLink} from "react-router-dom"
+import {NavItems} from "./navItems/NavItems"
 import Logo from "../../images/logo.svg"
 import {CgMenuLeftAlt as MenuIcon} from "react-icons/cg";
 const Navbar = () => {
-    
+    const [clicked, setClicked] = useState(false);
     return (
         <nav>
             
             <img src={Logo} alt=""/>
             <div className="nav-wrapper">
-            <NavLink className='nav-link' to='/'>Home</NavLink>
-            <NavLink className='nav-link' to='/login'>Login</NavLink>
-            <NavLink className='nav-link' to='/register'>Register</NavLink>
+             <MenuIcon />   
+            {NavItems.map((item, i) => {
+                return(
+                <NavLink key={i} className={item.cName} to={item.url}>{item.title}</NavLink>
+                )
+            })}
             
             </div>
            
---
+++ client/src/components/navbar/navItems/NavItems.js
@@ -0,0 +1,19 @@
+import Home from "../../home/Home"
+
+export const NavItems = [
+    {
+        title: "Home",
+        url: '/',
+        cName: 'nav-link'
+    },
+    {
+        title: "Login",
+        url: '/login',
+        cName: 'nav-link'
+    },
+    {
+        title: "Register",
+        url: '/register',
+        cName: 'nav-link'
+    },
+]
